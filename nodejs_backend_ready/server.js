12b2e756c3297459a7488ede66bf53b41a14d6e5
Akhil Mishra <akhilmishra544@gmail.com>
2025-05-19 10:20:42 +0530

    deploy-ready backend with root route

--- nodejs_backend_ready/server.js
+++ nodejs_backend_ready/server.js
@@ -1,20 +1,22 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
+// Default route for Render test
 app.get("/", (req, res) => {
   res.send("Server is working fine!");
 });
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
